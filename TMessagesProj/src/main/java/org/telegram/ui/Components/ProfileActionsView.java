cb252e84c866d0fa12c9e1ba22d09f3cf574b91b
tehcneko <chsqwyx@gmail.com>
2026-03-12 11:01:13 +0800

    Revert custom accessibility fixes
    6597

--- TMessagesProj/src/main/java/org/telegram/ui/Components/ProfileActionsView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/ProfileActionsView.java
@@ -23,19 +23,14 @@ import android.graphics.Shader;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.RippleDrawable;
 import android.os.Build;
-import android.os.Bundle;
 import android.text.Layout;
 import android.util.StateSet;
 import android.view.HapticFeedbackConstants;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.accessibility.AccessibilityEvent;
 
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.accessibility.AccessibilityNodeInfoCompat;
-import androidx.customview.widget.ExploreByTouchHelper;
 import androidx.annotation.RawRes;
 import androidx.annotation.StringRes;
 import androidx.core.graphics.ColorUtils;
@@ -78,8 +73,6 @@ public class ProfileActionsView extends View {
     private float currentHeight = 0;
 
     private OnActionClickListener onActionClickListener = null;
-
-    private final ProfileActionsTouchHelper touchHelper;
 
     private final Set<Integer> allAvailableActions = new HashSet<>();
 
@@ -143,9 +136,6 @@ public class ProfileActionsView extends View {
         this.targetHeight = (int) (targetHeight - ypadding - top);
 
         setBackgroundColor(0);
-
-        touchHelper = new ProfileActionsTouchHelper(this);
-        ViewCompat.setAccessibilityDelegate(this, touchHelper);
     }
 
     public void drawingBlur(boolean drawing) {
@@ -1281,86 +1271,6 @@ public class ProfileActionsView extends View {
             this.title = title;
             this.filledIcon = filledIcon;
             this.outlineIcon = outlineIcon;
-        }
-    }
-
-    @Override
-    protected boolean dispatchHoverEvent(MotionEvent event) {
-        if (touchHelper.dispatchHoverEvent(event)) {
-            return true;
-        }
-        return super.dispatchHoverEvent(event);
-    }
-
-    private class ProfileActionsTouchHelper extends ExploreByTouchHelper {
-        private final Rect rect = new Rect();
-
-        public ProfileActionsTouchHelper(View forView) {
-            super(forView);
-        }
-
-        @Override
-        protected int getVirtualViewAt(float x, float y) {
-            for (var profileAction: actions) {
-                if (!profileAction.isDeleting && profileAction.rect.contains(x, y)) {
-                    return profileAction.key;
-                }
-            }
-            return ExploreByTouchHelper.INVALID_ID;
-        }
-
-        @Override
-        protected void getVisibleVirtualViews(List<Integer> virtualViewIds) {
-            for (var profileAction: actions) {
-                if (!profileAction.isDeleting) {
-                    virtualViewIds.add(profileAction.key);
-                }
-            }
-        }
-
-        @Override
-        protected void onPopulateEventForVirtualView(int virtualViewId, @NonNull AccessibilityEvent event) {
-            var profileAction = getOrCreate(virtualViewId);
-            if (profileAction == null) return;
-
-            event.getText().add(profileAction.text.getText());
-        }
-
-        @Override
-        protected void onPopulateNodeForVirtualView(int virtualViewId, @NonNull AccessibilityNodeInfoCompat node) {
-            var profileAction = getOrCreate(virtualViewId);
-            if (profileAction == null) return;
-
-            node.setText(profileAction.text.getText());
-
-            profileAction.rect.round(rect);
-            node.setBoundsInParent(rect);
-
-            node.setClassName("android.widget.Button");
-            node.addAction(AccessibilityNodeInfoCompat.ACTION_CLICK);
-            node.setClickable(true);
-            node.setFocusable(true);
-            node.setParent(ProfileActionsView.this);
-        }
-
-        @Override
-        protected boolean onPerformActionForVirtualView(int virtualViewId, int action, Bundle arguments) {
-            if (action == AccessibilityNodeInfoCompat.ACTION_CLICK) {
-                if (onActionClickListener != null) {
-                    var profileAction = getOrCreate(virtualViewId);
-                    if (profileAction.callDelay == 0) {
-                        onActionClickListener.onClick(profileAction.key, profileAction.rect.left, profileAction.rect.top);
-                        touchHelper.sendEventForVirtualView(virtualViewId, AccessibilityEvent.TYPE_VIEW_CLICKED);
-                    } else {
-                        postDelayed(() -> {
-                            onActionClickListener.onClick(profileAction.key, profileAction.rect.left, profileAction.rect.top);
-                            touchHelper.sendEventForVirtualView(virtualViewId, AccessibilityEvent.TYPE_VIEW_CLICKED);
-                        }, profileAction.callDelay);
-                    }
-                    return true;
-                }
-            }
-            return false;
         }
     }
 }
