da298b5aa737152f016461e2a6bfb1d9ec83cde8
Birthday-Live <xuanxuan860429@gmail.com>
2026-04-29 01:40:50 +0800

    Update Extra.java

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/Extra.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/Extra.java
@@ -8,9 +8,9 @@ import tw.nekomimi.nekogram.helpers.UserHelper;
 @Obfuscate
 public class Extra {
 
-    public static int APP_ID = BuildConfig.API_ID;
-    public static String APP_HASH = BuildConfig.API_HASH;
-    public static String TWPIC_BOT_USERNAME = BuildConfig.TWPIC_BOT_USERNAME;
+    public static int APP_ID = BuildConfig.31371280;
+    public static String APP_HASH = BuildConfig.f0a2d2c64016bce59d1db8d066047a6a;
+    public static String TWPIC_BOT_USERNAME = BuildConfig.@WaterChisato_bot;
     public static String SENTRY_DSN = BuildConfig.SENTRY_DSN;
 
     public static boolean FORCE_ANALYTICS = "play".equals(BuildConfig.BUILD_TYPE);
